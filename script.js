596a65d69b396d2f39042a6c42d4a1390bf7167f
ballpanuwat25 <ballpanuwat25@gmail.com>
2025-03-04 20:14:19 +0700

    add modal

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
-    
+    // $('#start').click(function() {
+    //     // window.location.href = './pages/question.html';
+    // });
+
+    $("#start").click(function() {
+        $("#myModal").fadeIn();
+    });
+
+    // ปิด Modal เมื่อกดปุ่ม X
+    $(".close").click(function() {
+        $("#myModal").fadeOut();
+    });
+
+    // ปิด Modal เมื่อคลิกข้างนอก
+    $(window).click(function(event) {
+        if ($(event.target).is("#myModal")) {
+            $("#myModal").fadeOut();
+        }
+    });
 });
